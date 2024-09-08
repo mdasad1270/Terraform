a34c73ecbbdf2b38a16f3b00e9ea21a5fab015bb
mdasad1270 <148677457+mdasad1270@users.noreply.github.com>
2024-09-08 13:14:28 +0530

    Create securityGroup.tf

---
+++ securityGroup.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "my_sg" {
+  name_prefix = "my-sg-"
+  description = "Allow SSH access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Update this for more restrictive access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
